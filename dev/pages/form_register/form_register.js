f7fe3bea6e9bfa13a4c4a4e278052f4fca472421
ybbt <yorik_boss@ukr.net>
2019-03-13 06:02:45 +0200

    add test variant of validation

--- dev/pages/form_register/form_register.js
+++ dev/pages/form_register/form_register.js
@@ -1,13 +1,55 @@
-document.querySelector("._forMove").addEventListener("click", myFunction);
-document.querySelector("._forBackMove").addEventListener("click", myFunctionBack);
+$("._forMove").on("click", myFunction);
+$("._forBackMove").on("click", myFunctionBack);
 
 function myFunction(e) {
-	console.log("hello");
-	document.querySelector("._move").classList.add("row-reg_icons__move");
+	// console.log("hello");
+	$("._move").addClass("row-reg_icons__move");
 }
 
 function myFunctionBack(e) {
-	console.log("hello2");
-	document.querySelector("._move").classList.remove("row-reg_icons__move");
+	// console.log("hello2");
+	$("._move").removeClass("row-reg_icons__move");
 }
 
+// document.querySelector("._postCode").addEventListener("blur", postCodeValid);
+// var emptyMess = "This field can't be empty";
+
+function postCodeValid(e) {
+	console.log("post");
+	if (valExist(this)) { // введено не число
+		// показать ошибку
+		//this.className = "error";
+		console.log(emptyMess);
+	}
+}
+
+$("._formReg").on("blur", "._inputValid", function(){
+	// console.log("hi");
+	var emptyMess = "This field can't be empty";
+	if (!valExist($(this))) {
+		console.log(emptyMess + " - " + $(this).attr('id'));
+	}
+})
+
+// function validate() {
+// 	var elemArr = null;
+
+// 	var year = document.querySelector("._year");
+// 	var email = document.querySelector("._email");
+// 	var password = document.querySelector("._pass");
+// 	var postCode = document.querySelector("._postCode");
+
+// 	var emptyMess = "This field can't be empty";
+
+// 	if (!valExist(email)) {
+// 		console.log(emptyMess);
+// 	}
+// }
+
+function valExist ($elem) {
+	// console.log($elem.val());
+	if ($elem.val() != "") {
+		return true;
+	}
+	else return false;
+}
