82bda1606c76ea0839252be81b1fb1c7aea3bcdd
ybbt <yorik_boss@ukr.net>
2019-03-15 05:13:29 +0200

    add empty validation

--- dev/pages/form_register/form_register.js
+++ dev/pages/form_register/form_register.js
@@ -1,35 +1,55 @@
-$("._forMove").on("click", myFunction);
-$("._forBackMove").on("click", myFunctionBack);
+// $("._forMove").on("click", myFunction);
+// $("._forBackMove").on("click", myFunctionBack);
 
-function myFunction(e) {
-	// console.log("hello");
-	$("._move").addClass("row-reg_icons__move");
-}
 
-function myFunctionBack(e) {
-	// console.log("hello2");
-	$("._move").removeClass("row-reg_icons__move");
-}
+// function myFunction(e) {
+// 	// console.log("hello");
+// 	$("._move").addClass("row-reg_icons__move");
+// }
+
+// function myFunctionBack(e) {
+// 	// console.log("hello2");
+// 	$("._move").removeClass("row-reg_icons__move");
+// }
+
+$("._move").on("click", function(){
+	if(event.target.classList.contains('_forMove')) {
+		$(this).addClass("row-reg_icons__move");
+	}else if(event.target.classList.contains('_forBackMove') ){
+		$(this).removeClass("row-reg_icons__move");
+	}
+});
+
+
 
 // document.querySelector("._postCode").addEventListener("blur", postCodeValid);
 // var emptyMess = "This field can't be empty";
 
-function postCodeValid(e) {
-	console.log("post");
-	if (valExist(this)) { // введено не число
-		// показать ошибку
-		//this.className = "error";
-		console.log(emptyMess);
-	}
-}
+// function postCodeValid(e) {
+// 	console.log("post");
+// 	if (valExist(this)) { // введено не число
+// 		// показать ошибку
+// 		//this.className = "error";
+// 		console.log(emptyMess);
+// 	}
+// }
 
-$("._formReg").on("blur", "._inputValid", function(){
-	// console.log("hi");
+$("._formReg").on("blur", "._inputValid", emptyValidation);
+$("._formReg").on("focus", "._inputValid", emptyValidation);
+
+function emptyValidation(){
 	var emptyMess = "This field can't be empty";
 	if (!valExist($(this))) {
-		console.log(emptyMess + " - " + $(this).attr('id'));
+		console.log(emptyMess + " - " + $(this).attr('class'));
+		
+		$(this).addClass("row-reg_input__invalid")
+				.siblings("._rowTooltip").removeClass("row-reg_tooltip__invis")
+											.addClass("row-reg_tooltip__vis")
+											.find("._tooltipText").html(emptyMess);
+	} else {
+		$(this).removeClass("row-reg_input__invalid").siblings("._rowTooltip").removeClass("row-reg_tooltip__vis").addClass("row-reg_tooltip__invis").find("._tooltipText").html("ok");
 	}
-})
+}
 
 // function validate() {
 // 	var elemArr = null;
